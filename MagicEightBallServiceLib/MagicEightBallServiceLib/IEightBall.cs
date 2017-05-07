d9b7ee8b2fb70255a79049d90c66d247869b0298
guimpo <nakaima.prk@gmail.com>
2017-05-07 19:12:00 -0300

    Implementar contrato.

---
+++ MagicEightBallServiceLib/MagicEightBallServiceLib/IEightBall.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+
+namespace MagicEightBallServiceLib
+{
+    [ServiceContract(Namespace = "http://MyCompany.com")]
+    public interface IEightBall
+    {
+        // Faça uma pergunta, receba uma resposta!
+        [OperationContract]
+        string ObtainAnswerToQuestion(string userQuestion);
+    }
+}
--- MagicEightBallServiceLib/MagicEightBallServiceLib/MagicEightBallService.cs
+++ MagicEightBallServiceLib/MagicEightBallServiceLib/MagicEightBallService.cs
@@ -1,6 +1,22 @@
-﻿namespace MagicEightBallServiceLib
+﻿using System;
+
+namespace MagicEightBallServiceLib
 {
-    public class MagicEightBallService
+    public class MagicEightBallService : IEightBall
     {
+        // Somente para apresentação.
+        public MagicEightBallService()
+        {
+            Console.WriteLine("The 8-Ball awaits your question...");
+        }
+
+        public string ObtainAnswerToQuestion(string userQuestion)
+        {
+            string[] answers = { "Future Uncertain", "Yes", "No",
+                                 "Hazy", "Ask again later", "Definitely" };            
+            // Retorna uma resposta aleatória
+            Random r = new Random();
+            return answers[r.Next(answers.Length)];
+        }
     }
 }
