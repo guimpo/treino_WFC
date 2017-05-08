bb0b997e537108c8720c1231d3517155dbb94f52
guimpo <nakaima.prk@gmail.com>
2017-05-07 22:07:04 -0300

    Configurar App.config.

---
+++ MagicEightBallServiceHost/MagicEightBallServiceHost/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ServiceModel;
+using MagicEightBallServiceLib;
+
+namespace MagicEightBallServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Console Based WCF Host *****");
+            Console.ReadLine();
+        }
+    }
+}
