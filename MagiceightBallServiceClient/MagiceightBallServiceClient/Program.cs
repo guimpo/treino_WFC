1901517156355960d72e669b3614e98aca699bf7
guimpo <nakaima.prk@gmail.com>
2017-05-08 00:38:06 -0300

    Instanciar cliente.

---
+++ MagiceightBallServiceClient/MagiceightBallServiceClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using MagiceightBallServiceClient.ServiceReference1;
+using System;
+
+namespace MagiceightBallServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("***** Ask the Magic 8 Ball *****\n");
+            using (EightBallClient ball = new EightBallClient())
+            {
+                Console.Write("Your question: ");
+                string question = Console.ReadLine();
+                string answer = ball.ObtainAnswerToQuestion(question);
+                Console.WriteLine("8-Ball says: {0}", answer);
+            }
+            Console.ReadLine();
+        }
+    }
+}
