201946ec46537d40ad33266107d7e3b33a04209e
guimpo <nakaima.prk@gmail.com>
2017-05-07 23:28:13 -0300

    Configurar troca de metadata.

--- MagicEightBallServiceHost/MagicEightBallServiceHost/Program.cs
+++ MagicEightBallServiceHost/MagicEightBallServiceHost/Program.cs
@@ -6,6 +6,22 @@ namespace MagicEightBallServiceHost
 {
     class Program
     {
+        static void DisplayHostInfo(ServiceHost host)
+        {
+            Console.WriteLine();
+            Console.WriteLine("***** Host Info *****");
+            foreach
+            (System.ServiceModel.Description.ServiceEndpoint se in host.Description.Endpoints)
+            {
+                Console.WriteLine("Address: {0}", se.Address);
+                Console.WriteLine("Binding: {0}", se.Binding.Name);
+                Console.WriteLine("Contract: {0}", se.Contract.Name);
+                Console.WriteLine();
+            }
+            Console.WriteLine("**********************");
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("***** Console Based WCF Host *****");
@@ -14,6 +30,7 @@ namespace MagicEightBallServiceHost
             {
                 // Iniciar o provedor e esperar por mensagens
                 serviceHost.Open();
+                DisplayHostInfo(serviceHost);
 
                 // Manter serviço executando até a tecla 'enter' ser precionada
                 Console.WriteLine("The service is ready.");
