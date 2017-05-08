5167b7ef09a9a9d46dd903b87e7f2ce1c0a2cdf1
guimpo <nakaima.prk@gmail.com>
2017-05-07 22:15:53 -0300

    Codificar instanciação do provedor.

--- MagicEightBallServiceHost/MagicEightBallServiceHost/Program.cs
+++ MagicEightBallServiceHost/MagicEightBallServiceHost/Program.cs
@@ -9,7 +9,18 @@ namespace MagicEightBallServiceHost
         static void Main(string[] args)
         {
             Console.WriteLine("***** Console Based WCF Host *****");
-            Console.ReadLine();
+
+            using (ServiceHost serviceHost = new ServiceHost(typeof(MagicEightBallService)))
+            {
+                // Iniciar o provedor e esperar por mensagens
+                serviceHost.Open();
+
+                // Manter serviço executando até a tecla 'enter' ser precionada
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press the Enter key to terminate service.");
+                Console.ReadLine();
+            }
+            
         }
     }
 }
